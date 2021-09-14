ea93d2e079b4009951c50ec64bbba3618b46647f
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-09-14 21:29:45 +0300

    update urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
+    url(r'^accounts/register$',views.register,name='register'),
+    url(r'^accounts/login$',views.login,name='login'),
 ]
