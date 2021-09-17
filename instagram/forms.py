e378afd67c2edef8f7856aeebe5c8d2a96fa0b32
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-09-17 07:20:51 +0300

    create forms

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.db.models.expressions import Col
 from django.forms import widgets
-from .models import User
+from .models import Comment, Post, Profile, User
+from instagram import models
 #create forms for the application
 class RegisterForm(forms.ModelForm):
     class Meta:
@@ -20,3 +22,24 @@ class LoginForm(forms.ModelForm):
         widgets={
             'password':forms.PasswordInput(),
         }
+        
+class PostForm(forms.ModelForm):
+    class Meta:
+        model=Post
+        exclude=['user','name']
+        widgets={
+            'caption':forms.Textarea(),
+        }
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['user_id','user','name']
+        widgets={
+            'bio':forms.Textarea(),
+            'image':forms.ImageField()
+        }
+class UpdateUserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name','last_name')
