9b27d9de9d41bc5116c16d91def6b2317e1c4a29
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-09-17 07:19:41 +0300

    update urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,8 +1,14 @@
 from . import views
-from django.conf.urls import include, url
+from django.conf.urls import url,include
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
-    url(r'^accounts/register$',views.register,name='register'),
-    url(r'^accounts/login$',views.login,name='login'),
+    url('^$',views.index,name='index'),
+    url('logout/$', LogoutView, {"next_page": '/'}),
+    url('tinymce/',include('tinymce.urls')),
+    url('^createpost/',views.createpost,name='createpost'),
+    url('^comment/(\d)/',views.comment,name="comment"),
+    url('^profile/(\d)/',views.showprofile,name="profile"),
+    url('^profile/update/(\d)/',views.updateprofile,name="updateprofile")
+    
 ]
